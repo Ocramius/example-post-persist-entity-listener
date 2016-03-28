dc7a5cbd5dc79b676139884b1014386b1a8d4cd3
Marco Pivetta <ocramius@gmail.com>
2016-03-29 00:53:22 +0200

    Example bootstrapper file

---
+++ bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace ExamplePostPersistEntityListener;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+use Doctrine\Common\Annotations\AnnotationRegistry;
+use Doctrine\DBAL\Driver\PDOSqlite\Driver;
+use Doctrine\ORM\Configuration;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Proxy\ProxyFactory;
+use Doctrine\ORM\Tools\SchemaTool;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+AnnotationRegistry::registerLoader('class_exists');
+
+$configuration = new Configuration();
+
+$configuration->setMetadataDriverImpl(new AnnotationDriver(new AnnotationReader()));
+$configuration->setProxyDir(sys_get_temp_dir() . '/' . uniqid('example', true));
+$configuration->setProxyNamespace('ProxyExample');
+$configuration->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_EVAL);
+
+$entityManager = EntityManager::create(
+    [
+        'driverClass' => Driver::class,
+        'path'        => __DIR__ . '/test-db.sqlite',
+    ],
+    $configuration
+);
+
+$schemaTool = new SchemaTool($entityManager);
+
+$schemaTool->updateSchema($entityManager->getMetadataFactory()->getAllMetadata());
+
+return $entityManager;
